8cd4eb61ee19a9e5d528029509bf6a01578a9eec
Florian Peters <fl0wryan@hotmail.com>
2021-05-15 18:41:54 +0200

    Add processing of items

--- src/DnDBeyondProxy.py
+++ src/DnDBeyondProxy.py
@@ -21,6 +21,14 @@ class DnDBeyondProxy:
         with open(os.path.join('..', 'meta', 'mapping.json'), mode='r', encoding='utf8') as fd:
             self._mapping = json.load(fd)
 
+        # Build mappings
+        self._stealth_map = {x['id']: x['name'] for x in self._mapping['stealthCheckTypes']}
+        self._attack_map = {x['id']: x['name'] for x in self._mapping['rangeTypes']}
+        self._category_map = {x['id']: x['name'] for x in self._mapping['weaponCategories']}
+        self._source_map = {x['id']: x['name'] for x in self._mapping['sources']}
+        self._armor_map = {x['id']: x['name'] for x in self._mapping['armorTypes']}
+        self._gear_map = {x['id']: x['name'] for x in self._mapping['gearTypes']}
+
     def _authenticate(self):
         if not self._token or self._token_death_timestamp <= time.time():
             logging.info('Requesting new bearer token')
@@ -33,8 +41,9 @@ class DnDBeyondProxy:
             except KeyError:
                 raise ConnectionError('Failed to authenticate using Cobalt key.')
 
-    def _dump_data(self, data, filename):
-        final_path = os.path.join(self._output_folder, 'raw')
+    def _dump_data(self, data, filename, raw=True):
+        data_type = 'raw' if raw else 'processed'
+        final_path = os.path.join(self._output_folder, data_type)
         Path(final_path).mkdir(parents=True, exist_ok=True)
         with open(os.path.join(final_path, filename), mode='w') as fd:
             json.dump(data, fd)
@@ -87,3 +96,42 @@ class DnDBeyondProxy:
                 raise RuntimeError('Failed to obtain spells.')
 
         self._dump_data(aggregator, 'spells.json')
+
+    def process_items(self, input_file=None):
+        if not input_file:
+            input_file = os.path.join('..', 'data', 'output', 'raw', 'items.json')
+        output_file = os.path.join(self._output_folder, 'processed', 'items.json')
+
+        # Read input file
+        with open(input_file, mode='r', encoding='utf8') as fd:
+            data = json.load(fd)
+
+        logging.info('Processing items')
+        result = [self._process_item(x) for x in data]
+
+        # Write to file
+        self._dump_data(result, 'items.json', raw=False)
+
+    def _process_item(self, item):
+        """ --- List of processed data ---
+            <in items.json> <=> <in mapping.json>
+            stealthCheck    <=> stealthCheckTypes
+            attackType      <=> rangeTypes (Most likely guess?)
+            categoryId      <=> weaponCategories
+            sourceId        <=> sources
+            armorTypeId     <=> armorTypes
+            gearTypeId      <=> gearTypes
+        """
+        logging.debug('Processing item {0}'.format(item['name']))
+
+        # Apply mappings
+        item['stealthCheck'] = self._stealth_map[item['stealthCheck']] if item['stealthCheck'] else None
+        item['attackType'] = self._attack_map[item['attackType']] if item['attackType'] else None
+        item['category'] = self._category_map[item['categoryId']] if item['categoryId'] else None
+        item['source'] = self._source_map[item['sourceId']] if item['sourceId'] else None
+        for source in item['sources']:
+            source['sourceName'] = self._source_map[source['sourceId']] if source['sourceId'] else None
+        item['armorType'] = self._armor_map[item['armorTypeId']] if item['armorTypeId'] else None
+        item['gearType'] = self._gear_map[item['gearTypeId']] if item['gearTypeId'] else None
+
+        return item
